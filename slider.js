d84bc378bbfbf97ca380f76906f558a0268e6986
tiper12624 <84010051+tiper12624@users.noreply.github.com>
2021-07-23 18:20:43 +0300

    Add swipe to slider

--- slider.js
+++ slider.js
@@ -1,19 +1,28 @@
-function sliderInit () {
-  const slider = document.querySelector('[class^=Landing_innerwrapper_opinions]')
-  const slides = document.querySelectorAll('[class^=Landing_innerwrapper_card]')
-  const controlsWrapper = document.querySelector('[class^=Landing_innerwrapper_arrow]')
+const slider = document.querySelector('[class^=Landing_innerwrapper_opinions]')
+const slides = document.querySelectorAll('[class^=Landing_innerwrapper_card]')
+const controlsWrapper = document.querySelector('[class^=Landing_innerwrapper_arrow]')
 
+let slideMargin = 0
+let slideWidth = 0
+let slideHeight = 0
+let slidesScreen = 0
+
+let sliderWrapper = null
+
+let x0 = null
+
+function sliderInit () {
   const slideStyle = slides[0].currentStyle || window.getComputedStyle(slides[0])
-  const slideMargin = parseInt(slideStyle.marginRight)
-  const slideWidth = slides[0].clientWidth + slideMargin
-  const slideHeight = slides[0].clientHeight
-  const slidesScreen = Math.floor(slider.clientWidth / slideWidth)
+  slideMargin = parseInt(slideStyle.marginRight)
+  slideWidth = slides[0].clientWidth + slideMargin
+  slideHeight = slides[0].clientHeight
+  slidesScreen = Math.floor(slider.clientWidth / slideWidth)
 
   slider.style.position = 'relative'
   slider.style.display = 'block'
   slider.style.height = `${slideHeight}px`
 
-  const sliderWrapper = initWrapper(slider, slides)
+  sliderWrapper = initWrapper()
   sliderWrapper.style.display = 'flex'
   sliderWrapper.style.width = `${sliderWrapper.children.length * slideWidth}px`
   sliderWrapper.style.transition = 'left .3s ease 0s'
@@ -24,7 +33,9 @@ function sliderInit () {
   sliderWrapper.setAttribute('data-index', '0')
 
   if (sliderWrapper.children.length > slidesScreen) {
-    const [slideLeft, slideRight] = initControls(controlsWrapper)
+    controlsWrapper.style.display = 'flex'
+
+    const [slideLeft, slideRight] = initControls()
     slideLeft.addEventListener('click', e => {
       e.preventDefault()
       scrollLeft(sliderWrapper, slideWidth)
@@ -37,12 +48,16 @@ function sliderInit () {
 
       return false
     })
+
+    // swipe events
+    slider.addEventListener('touchstart', startSwipe)
+    slider.addEventListener('touchend', endSwipe)
   } else {
-    controlsWrapper.remove()
+    controlsWrapper.style.display = 'none'
   }
 }
 
-function initWrapper (slider, slides) {
+function initWrapper () {
   const wrapper = document.createElement('div')
   slides.forEach((slide, index) => {
     if (index < 5) {
@@ -55,7 +70,7 @@ function initWrapper (slider, slides) {
   return wrapper
 }
 
-function initControls (controlsWrapper) {
+function initControls () {
   const controlsImg = controlsWrapper.querySelectorAll('img')
 
   const slideLeft = document.createElement('a')
@@ -77,26 +92,51 @@ function initControls (controlsWrapper) {
   return [slideLeft, slideRight]
 }
 
-function scrollLeft (sliderWrapper, slideWidth, n = 1) {
+function scrollLeft (n = 1) {
   const count = parseInt(sliderWrapper.getAttribute('data-count'))
   const screen = parseInt(sliderWrapper.getAttribute('data-screen'))
   let index = parseInt(sliderWrapper.getAttribute('data-index')) - n
   if (index < 0) {
     index = count - screen
   }
-  goToSlide(sliderWrapper, slideWidth, index)
+  goToSlide(index)
 }
 
-function scrollRight (sliderWrapper, slideWidth, n = 1) {
+function scrollRight (n = 1) {
   const count = parseInt(sliderWrapper.getAttribute('data-count'))
   const screen = parseInt(sliderWrapper.getAttribute('data-screen'))
   let index = parseInt(sliderWrapper.getAttribute('data-index')) + n
-  goToSlide(sliderWrapper, slideWidth, index % (count - screen + 1))
+  goToSlide(index % (count - screen + 1))
 }
 
-function goToSlide (sliderWrapper, slideWidth, index) {
+function goToSlide (index) {
   sliderWrapper.setAttribute('data-index', index.toString())
   sliderWrapper.style.left = `-${slideWidth * index}px`
+}
+
+function changedTouches (e) {
+  return e.changedTouches ? e.changedTouches[0] : e
+}
+
+function startSwipe (e) {
+  e.preventDefault()
+  x0 = changedTouches(e).clientX
+
+  return false
+}
+
+function endSwipe (e) {
+  if (x0 !== null) {
+    let dx = changedTouches(e).clientX - x0
+
+    if (Math.sign(dx) > 0) {
+      scrollLeft()
+    } else {
+      scrollRight()
+    }
+
+    x0 = null
+  }
 }
 
 document.addEventListener('DOMContentLoaded', sliderInit)
